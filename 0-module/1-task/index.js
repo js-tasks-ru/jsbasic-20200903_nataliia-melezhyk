78763de64ebd76f82618ffa863c30f4d27d6acf8
Nikolay Melezhik <nikolay.melezhik@nikolay.melezhik>
2020-09-03 10:21:00 +0300

    addition two numbers

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,6 +4,19 @@
  * @param {number} n
  * @returns {number}
  */
+// function sum(m, n) {
+//   let result = m + n;
+//   return result;
+//   // return m + n;
+// }
+
+// sum(1,1);
+// sum(2,5);
+
 function sum(m, n) {
-  // ваш код...
+  let result = m + n;
+  return result;
 }
+
+sum(1,1);
+sum(2,5);
