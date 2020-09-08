cbb78522422badf81df0f15766af3c9185ad8d34
nataliiamelezhik <nikolay.melezhik@nikolay.melezhik>
2020-09-08 15:00:50 +0300

    Factorial

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -8,3 +8,6 @@ function sum(m, n) {
   let result = m + n;
   return result;
 }
+
+sum(1,1);
+sum(2,5);
--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,19 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let counter = n;
+  let result = n;
+  if( n === 1 || n === 0 ) result = 1;
+  else{
+    for(let i = 1; i <= counter; i++){
+      result = result * (n-1);
+      counter--;
+      n--;
+    }
+  }
+  return result;
 }
+factorial(0); 
+factorial(1);  
+factorial(3);  
+factorial(5);
