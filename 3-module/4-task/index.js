7bebdb4b3d8d8566ae702db3ee24510c7c51a072
nataliiamelezhik <nikolay.melezhik@nikolay.melezhik>
2020-09-17 23:01:48 +0300

    showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -5,5 +5,13 @@
  * @returns {string}
  */
 function showSalary(users, age) {
-  // ваш код...
+  let maxAge = age;
+  let data = '';
+  users.forEach((item) => {
+    if(item.age <= maxAge){
+      data += item.name + ", " + item.balance + "\n";
+    }
+  });
+  data = data.trim();
+  return data;
 }
