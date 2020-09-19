0cb118f4762133549b53d73642e2886c969a16ae
nataliiamelezhik <nikolay.melezhik@nikolay.melezhik>
2020-09-19 14:28:49 +0300

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,20 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  /*const listFiends = document.createElement('ul');
+  for(let item of friends){
+    const itemFriends = document.createElement('li');
+    itemFriends.innerHTML =  item.firstName + item.lastName ;
+    listFiends.append(itemFriends); 
+  }
+  document.body.append(listFiends);*/
+
+  const listFiends = document.createElement('ul');
+  friends.forEach((item) => {
+    const itemFriends = document.createElement('li');
+    itemFriends.innerHTML =  item.firstName + item.lastName ;
+    listFiends.append(itemFriends); 
+  })
+  console.log(listFiends);
+  return listFiends;
 }
